beea12b9ad05b4828a918f4a849360c985f578e5
William <mack.mindenhall@gmail.com>
2020-07-01 10:58:33 +0000

    Added a constants.js file

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 // Establishes a connection with the game server
 
 const connect = function () {
+  // const { IP, PORT } = constants;
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
+    IP,
+    PORT,
   });
   // Interpret incoming data as text
   conn.setEncoding("utf8");
@@ -16,26 +18,6 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: ___");
-    // conn.write("Move: up");
-    // setTimeout(() => {
-    //   conn.write("Move: down");
-    // }, 500);
-    // setTimeout(() => {
-    //   conn.write("Move: down");
-    // }, 1000);
-    // setTimeout(() => {
-    //   conn.write("Move: left");
-    // }, 1500);
-    // setTimeout(() => {
-    //   conn.write("Move: left");
-    // }, 2000);
-    // conn.write("Move: down");
-    // conn.write("Move: left");
-    // conn.write("Move: left");
-    // conn.write("Move: left");
-    // conn.write("Move: left");
-    // conn.write("Move: left");
-    // conn.write("Move: left");
   });
 
   return conn;
